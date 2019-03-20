64fd12047110d8b4c8d81c0358f9b1c5df4a2f28
yuguangchao <yuguangchao@secoo.com>
2019-03-20 15:18:14 +0800

    上传失败提示

--- src/pages/Match/uploadMatch.js
+++ src/pages/Match/uploadMatch.js
@@ -22,7 +22,11 @@ class GuoMeiUploadFile extends PureComponent {
           console.log(info.file, info.fileList);
         }
         if (info.file.status === 'done') {
-          message.success(`${info.file.name} file uploaded successfully`);
+          if (info.file.response.data.status === 'ok') {
+            message.success(`${info.file.name} file uploaded successfully`);
+          } else {
+            message.error(`${info.file.name} file upload failed.`);
+          }
         } else if (info.file.status === 'error') {
           message.error(`${info.file.name} file upload failed.`);
           console.log(`${info.file.response.message}`);
