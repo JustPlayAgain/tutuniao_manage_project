9cab59fd73827bf7f3b83b359a8af5e897450e7b
yuguangchao <yuguangchao@secoo.com>
2019-03-23 23:17:40 +0800

    header

--- src/utils/request.js
+++ src/utils/request.js
@@ -153,7 +153,13 @@ export default function request(url, option) {
       sessionStorage.removeItem(`${hashcode}:timestamp`);
     }
   }
-
+  newOptions.headers = {
+    ...newOptions.headers,
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+    'Access-Control-Allow-Methods': 'POST, GET, PUT',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+  };
   return fetch(url, newOptions)
     .then(checkStatus)
     .then(response => cachedSave(response, hashcode))
