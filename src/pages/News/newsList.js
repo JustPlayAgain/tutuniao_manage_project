47e48dc2c3ebbdf27b0b02fea236815839f5e28f
yuguangchao <yuguangchao@secoo.com>
2019-04-29 21:21:49 +0800

    修改新闻

--- src/pages/News/newsList.js
+++ src/pages/News/newsList.js
@@ -158,15 +158,18 @@ class NewsList extends PureComponent {
     {
       title: '跳转链接',
       dataIndex: 'newsUrl',
+      fixed: 'right',
       render: val => <a href={val}>跳转URL</a>,
       key: 'newsUrl',
     },
     {
       title: '创建人',
+      fixed: 'right',
       dataIndex: 'createUser',
     },
     {
       title: '创建时间',
+      fixed: 'right',
       sorter: true,
       dataIndex: 'createDate',
       render: val => <span>{moment(val).format('YYYY-MM-DD')}</span>,
@@ -183,6 +186,7 @@ class NewsList extends PureComponent {
     // },
     {
       title: '操作',
+      fixed: 'right',
       render: (text, record) => (
         <Fragment>
           <a onClick={() => this.handleUpdateModalVisible(true, record)}>修改</a>
